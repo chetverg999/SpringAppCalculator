18d6c3d4d21076df0dd98e5faba34966cbc59573
Alexandr <s.iva2303@yandex.ru>
2022-10-21 23:27:20 +0300

    commit 5

--- src/main/java/ru/ivanov/spring/config/SpringConfig.java
+++ src/main/java/ru/ivanov/spring/config/SpringConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
