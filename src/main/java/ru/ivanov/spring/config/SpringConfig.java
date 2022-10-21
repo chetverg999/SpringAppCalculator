0b1b91a9ac0ebf5bcc7d3ae8e39049d6ae45d42f
Alexandr <s.iva2303@yandex.ru>
2022-10-21 19:34:42 +0300

    commit 3

--- src/main/java/ru/ivanov/spring/config/SpringConfig.java
+++ src/main/java/ru/ivanov/spring/config/SpringConfig.java
@@ -5,12 +5,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Ivanov Alexandr on 06.10.2022
@@ -31,6 +32,7 @@ public class SpringConfig implements WebMvcConfigurer {
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setCharacterEncoding("UTF-8"); // forcing UTF-8
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
@@ -38,17 +40,56 @@ public class SpringConfig implements WebMvcConfigurer {
     }
 
     @Bean
+    public SpringResourceTemplateResolver templatecssResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setCharacterEncoding("UTF-8"); // forcing UTF-8
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/resources/css/");
+        templateResolver.setSuffix(".css");
+        return templateResolver;
+    }
+
+    @Bean
     public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.addTemplateResolver(templatecssResolver());
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+//        viewResolver.setCharacterEncoding("UTF-8"); // forcing UTF-8
+        viewResolver.setContentType("text/html; charset=UTF-8");
+        viewResolver.setOrder(1);
+        viewResolver.setViewNames(new String[] { ".html", ".xhtml" });
+        return viewResolver;
     }
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8"); // <- this was added
+        resolver.setForceContentType(true); // <- this was added
+        resolver.setContentType("text/html; charset=UTF-8"); // <- this was added
         registry.viewResolver(resolver);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/resources/css/");
+    }
+
+//    @Override
+//    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//        configurer.enable();
+//    }
+
+
 }
--- src/main/java/ru/ivanov/spring/dao/ExpressionDAO.java
+++ src/main/java/ru/ivanov/spring/dao/ExpressionDAO.java
@@ -11,6 +11,7 @@ import java.util.List;
  */
 
 @Component
+
 public class ExpressionDAO {
     private List<Expression> exampleList;
     private List<LogicExpression> logicExampleList;
